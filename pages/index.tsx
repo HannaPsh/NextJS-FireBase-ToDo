452845c87bcff4496e04dab4327dfb5c7778da9d
HannaPsh <hannapshanich@gmail.com>
2023-05-01 19:59:44 +0200

    added get,add,uppdate and delete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,139 @@
+import { useEffect, useState } from 'react';
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { firestore } from '../firebase/clientApp';
+import {updateDoc} from "@firebase/firestore";
+import { doc } from '@firebase/firestore'; // for creating a pointer to our Document  
+import {deleteDoc} from "@firebase/firestore";
+import {collection,QueryDocumentSnapshot,DocumentData,query,where,limit,getDocs} from "@firebase/firestore";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const Home: NextPage = () => {
+const todosCollection = collection(firestore,'todos');
+const [todos,setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+const [dones,setDones] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+const [loading,setLoading] = useState<boolean>(true);
+const getTodos = async () => {
+  const todosQuery = query(todosCollection,where('done','==',false),limit(10));
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const querySnapshot = await getDocs(todosQuery);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const result: QueryDocumentSnapshot<DocumentData>[] = [];
+  querySnapshot.forEach((snapshot) => {
+  result.push(snapshot);
+  });
+  setTodos(result);
+};
+const getDones = async () => {
+  const todosQuery = query(todosCollection,where('done','==',true),limit(10));
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  const querySnapshot = await getDocs(todosQuery);
+ 
+  const result: QueryDocumentSnapshot<DocumentData>[] = [];
+  querySnapshot.forEach((snapshot) => {
+  result.push(snapshot);
+  });
+  setDones(result);
+};
+const updateTodo = async (documentId: string) => {   
+  // create a pointer to the Document id
+  const _todo = doc(firestore,`todos/${documentId}`);
+  await updateDoc(_todo,{
+    "done":true
+  });
+  getTodos();
 }
+
+const deleteTodo = async (documentId:string) => {
+
+  const _todo = doc(firestore,`todos/${documentId}`);
+
+  await deleteDoc(_todo);
+ 
+  getTodos();
+}
+
+useEffect( () => {
+
+  getTodos();
+  getDones()
+
+  setTimeout( () => {
+    setLoading(false);
+  },2000)
+},[todos,dones]);
+return (
+   <div className={styles.container}>
+   <Head>
+     <title>Todos app</title>
+     <meta name="description" content="Next.js firebase todos app" />
+     <link rel="icon" href="/favicon.ico" />
+   </Head>
+   <main className={styles.main}>
+     <h1 className={styles.title}>
+     Todos app
+     </h1>
+     <p>Consider adding a todo from <a id='addNewLink' href="/add-todo">here</a></p>
+     <div className={styles.grid}>
+     {loading ? (
+    <div className={styles.card}>
+      <h2>Loading</h2>
+    </div>
+  ) : todos.length === 0 ? (
+    <div className={styles.card}>
+      <h2>No undone todos</h2>
+    </div>
+  ) : (
+    <>
+      <div className={styles.todos}>
+        <h2>Undone Todos</h2>
+        {todos.map((todo) => {
+          const data = todo.data();
+          return (
+            <div className={styles.card}>
+              <h2>{data.title}</h2>
+              <p>{data.description}</p>
+              <div className={styles.cardActions}>
+                <button type="button" onClick={() => updateTodo(todo.id)}>Mark as done</button>
+                <button type="button" onClick={() => deleteTodo(todo.id)}>Delete</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.todos}>
+        <h2>Done Todos</h2>
+        {dones.map((done) => {
+          const data = done.data();
+          return (
+            <div className={styles.card}>
+              <h2>{data.title}</h2>
+              <p>{data.description}</p>
+              <div className={styles.cardActions}>
+            <span>Done!</span>
+                <button type="button" onClick={() => deleteTodo(done.id)}>Delete</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  )}
+</div> 
+   </main>
+   <footer className={styles.footer}>
+     <a
+     href="#"
+     rel="noopener noreferrer"
+     >
+     Todos app
+     </a>
+   </footer>
+   </div>
+)
+}
+export default Home
